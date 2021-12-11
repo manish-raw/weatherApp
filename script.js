3372a390082485a0295d9eb434bdeeef7be6f198
Manish Amarnath Yadav <manishyadav.y31@gmail.com>
2021-12-11 20:43:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const inputTxt = document.querySelector('.inputTxt');
+var button = document.querySelector('.btn');
+const showData = document.querySelector('.showData');
+const API_Key = "9e33b9f4e5f351a40210e6e32252a463";
+
+button.addEventListener('click', () => {
+    const cityInput = inputTxt.value;
+    //console.log(cityInput);
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=metric&APPID=${API_Key}`)
+    .then(res => res.json())
+    .then(data => {
+        //console.log(data);
+        inputTxt.value = " ";
+        showData.innerHTML = `
+                            <ul>
+                                   
+                                <li class="city">${data.name},${data.sys.country}</li>
+                                <li class="desc">${data.weather[0].description}</li>
+                                <li class="temp">${data.main.temp}°C</li>    
+                            </ul>
+
+                            `;
+        });
+});
